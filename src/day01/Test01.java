b0d34b9c4c0be50fe8e1e518b1e48631311267e7
sehyeon <sehyeon111@naver.com>
2022-05-13 16:16:31 +0900

    집에서 작업

--- src/day01/Test01.java
+++ src/day01/Test01.java
@@ -3,7 +3,8 @@ package day01;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("집에서 작업했습니다");
+		System.out.println("안녕하세용");
 
 	}
 
